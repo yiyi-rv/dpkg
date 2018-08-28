5b967918ec7adb61dfb3c5ea5d1ddf9f8e7ae0ad
yiyiwang <shd101wyy@sina.com>
2018-08-28 14:18:09 -0500

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,31 @@
+set -x
+
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+rm $json_out
+rm -rf $report_out
+
+# This file is modified according to .gitlab-ci.yml
+apt update -qq
+apt install -qq -y eatmydata autoconf autopoint gettext
+eatmydata apt install -qq -y --no-install-recommends git ca-certificates
+eatmydata apt build-dep -qq -y .
+./autogen
+
+eatmydata apt install -qq -y --no-install-recommends libmodule-build-perl
+eatmydata apt install -qq -y --no-install-recommends \
+      fakeroot gpg cppcheck aspell aspell-en i18nspector \
+      libtest-strict-perl libtest-minimumversion-perl libtest-perl-critic-perl \
+      libtest-pod-perl libtest-pod-coverage-perl libtest-spelling-perl \
+      libtest-synopsis-perl
+
+./configure CC=kcc LD=kcc CFLAGS="-fissue-report=$json_out" 
+
+make -j`nproc`
+# make distcheck
+# make check VERBOSE=1 TESTSUITEFLAGS=--verbose TEST_PARALLEL=$(nproc) AUTHOR_TESTING=1
+# mkdir -p build-tree
+# cd build-tree
+# make check VERBOSE=1 TESTSUITEFLAGS=--verbose TEST_PARALLEL=$(nproc)
+
+touch $json_out && rv-html-report $json_out -o $report_out
